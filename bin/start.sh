69512b0d285c03eb14553e12f2780bf781a81233
Nazim Lachter <lachtater@gmail.com>
2019-01-30 02:28:34 +0100

    Simplified start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-#COMMAND=$(node ./node_modules/kiwi-bundle/lib/utils/output-cmd-webpack-start.js $PWD)
-
 NM="./node_modules"
 BIN="$NM/.bin/webpack-dev-server"
 BUNDLE="$NM/kiwi-bundle"
 CONFIG="$BUNDLE/etc/webpack/development.js"
 
-COMMAND="$BIN --context=$BUNDLE --config=$CONFIG"
-
-eval $COMMAND
+eval $BIN --context=$BUNDLE --config=$CONFIG
