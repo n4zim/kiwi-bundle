cbb09a27f4d3424af6e58812569615fffc424a92
Nazim Lachter <lachtater@gmail.com>
2019-06-21 14:06:35 +0200

    Minor upgrade from 2.1.0 to 2.2.0

--- lib/client/routes/Route.d.ts
+++
@@ -1,6 +0,0 @@
-import { WebPageConstructor } from "../components/WebPage";
-export default class Route {
-    path: string;
-    component: WebPageConstructor;
-    constructor(path: string, component: WebPageConstructor);
-}
--- lib/client/storage/Database.d.ts
+++
@@ -1,9 +0,0 @@
-import Repository from "./Repository";
-declare class Database {
-    name: string;
-    private repositories;
-    constructor(name: string, repositories: Repository[]);
-    onUpgradeNeeded(database: IDBDatabase): void;
-    onSuccess(database: IDBDatabase): void;
-}
-export default Database;
