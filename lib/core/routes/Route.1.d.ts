ec509b15f5cb87102eafd456b8c43521a4e8c0ac
Nazim Lachter <lachtater@gmail.com>
2019-07-08 11:14:51 +0200

    Minor upgrade from 2.2.0 to 2.3.0

--- lib/core/routes/Route.1.d.ts
+++
@@ -1,7 +0,0 @@
-import { WebPageConstructor } from "../components/WebPage";
-export default class Route {
-    path: string;
-    component: WebPageConstructor;
-    constructor(path: string, component: WebPageConstructor);
-}
-//# sourceMappingURL=Route.1.d.ts.map
