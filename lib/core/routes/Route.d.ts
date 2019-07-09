833abc4a296765819b9f0a04694e5872dfe87266
Jeremy Levy <jeremy.levy@zei-world.com>
2019-07-09 15:00:49 +0200

    Ajout des options dans l'entité route

--- lib/core/routes/Route.d.ts
+++ lib/core/routes/Route.d.ts
@@ -1,7 +1,12 @@
 import { WebPageConstructor } from "../components/WebPage";
+interface RouteOptions {
+    authLevels?: Array<string>;
+}
 export default class Route {
     path: string;
     component: WebPageConstructor;
-    constructor(path: string, component: WebPageConstructor);
+    options: RouteOptions;
+    constructor(path: string, component: WebPageConstructor, options?: RouteOptions);
 }
+export {};
 //# sourceMappingURL=Route.d.ts.map
--- src/core/routes/Route.ts
+++ src/core/routes/Route.ts
@@ -1,12 +1,20 @@
 import { WebPageConstructor } from "../components/WebPage"
 
+interface RouteOptions {
+  authLevels?: Array<string>
+}
+
 export default class Route {
   path: string
   component: WebPageConstructor
 
-  constructor(path: string, component: WebPageConstructor) {
+  options: RouteOptions
+
+  constructor(path: string, component: WebPageConstructor, options: RouteOptions = {}) {
     this.path = path
     this.component = component
+
+    this.options = options
   }
 
 }
