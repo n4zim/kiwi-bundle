a1ac9ef3cd75c4dff5e59faf1c98277aadb12143
Nazim Lachter <nazim@blueforest.fr>
2019-07-14 16:51:17 +0200

    Finishing i18n

--- src/core/client/i18n.ts
+++ src/core/client/i18n.ts
@@ -1,4 +1,5 @@
-import { Name } from "dropin-recipes"
+import { Name, Languages, NameByNumber } from "dropin-recipes"
+import { logger } from "."
 
 export type i18nStrings = { [name: string]: Name }
 
@@ -8,5 +9,35 @@ export function i18n(data: Name, count = 1, vars = {}): string {
   if(typeof data === "string") {
     return data
   }
-  return ""
+
+  let current: string | NameByNumber = ""
+
+  // Is NameByLanguage
+  if(typeof data === "object") {
+
+    // Global text is set
+    if(typeof data[Languages.ANY] !== "undefined") {
+      current = data[Languages.ANY]
+
+    // Current language
+    } else if(typeof data[currentLanguage] !== "undefined") {
+      current = data[currentLanguage]
+    }
+
+    // Is NameByNumber
+    if(typeof current === "object") {
+      if(count === 1) {
+        if(typeof current.one !== "undefined") {
+          return current.one
+        }
+      } else if(typeof current.many !== "undefined") {
+        return current.many
+      }
+    }
+
+  } else {
+    logger.logError("i18n", "Unknown i18n data type")
+  }
+
+  return current as string
 }
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,4 @@
-export * from "./client/Client"
+export * from "./client"
 export * from "./components"
 export * from "./routes"
 export * from "./storage"
