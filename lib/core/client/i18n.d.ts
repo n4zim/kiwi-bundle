9a02e6867cdf9076b6723a0c6872973e5f02187b
Nazim Lachter <nazim@blueforest.fr>
2019-08-01 19:10:58 +0200

    Replacing i18nStrings by MultipleNames

--- lib/core/client/i18n.d.ts
+++ lib/core/client/i18n.d.ts
@@ -1,7 +1,4 @@
 import { Name } from "dropin-recipes";
-export declare type i18nStrings = {
-    [name: string]: Name;
-};
-export declare const currentLanguage: any;
+export declare const currentLanguage: import("dropin-recipes").LanguageSelector;
 export declare function i18n(name: Name, count?: number, vars?: {}): string;
 //# sourceMappingURL=i18n.d.ts.map
--- src/core/client/i18n.ts
+++ src/core/client/i18n.ts
@@ -1,7 +1,5 @@
 import { stringToLanguageSelector, Name, nameToString } from "dropin-recipes"
 
-export type i18nStrings = { [name: string]: Name }
-
 export const currentLanguage = stringToLanguageSelector(navigator.language.slice(0, 2))
 
 export function i18n(name: Name, count = 1, vars = {}): string {
