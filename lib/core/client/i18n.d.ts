cafbdb9a837e4970b8731e8ef03d6f2b3480237c
Nazim Lachter <lachtater@gmail.com>
2019-08-15 16:23:50 +0200

    Patch upgrade from 2.7.2 to 2.7.3

--- lib/core/client/i18n.d.ts
+++
@@ -1,4 +0,0 @@
-import { Name } from "dropin-recipes";
-export declare const currentLanguage: import("dropin-recipes").Language;
-export declare function i18n(name: Name, count?: number, vars?: {}): string;
-//# sourceMappingURL=i18n.d.ts.map
