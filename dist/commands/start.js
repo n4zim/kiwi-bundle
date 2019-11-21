c968b60dbc84f4c844501d0e69652d47f58ab8d1
Nazim Lachter <lachtater@gmail.com>
2019-11-21 17:29:13 +0100

    Removing lib dir

--- dist/commands/start.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var context_1 = require("../core/context");
-var tsc_1 = require("../core/tsc");
-exports.Start = function (path) {
-    var context = new context_1.KiwiBundleContext(path);
-    context.display();
-    tsc_1.TypeScriptComplier.watch(context);
-};
