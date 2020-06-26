a6d923913bb653c495c13620239f5c74d65beb25
Nazim Lachter <nazim@blueforest.cc>
2020-06-26 18:56:34 +0200

    Sandboxing

--- src/commands/postinstall.ts
+++ src/commands/postinstall.ts
@@ -1,13 +1,13 @@
 import commandExists from "command-exists"
+import chalk from "chalk"
 import { exec } from "child_process"
 import { Bundle } from "../core/bundle"
-import chalk from "chalk"
 
-const addDeps = (deps: { [name: string]: string }, ignore: string[]) => {
+const addDeps = (deps: { [name: string]: string }|undefined, ignoreList: string[]) => {
   if(typeof deps !== "undefined") {
     Object.keys(deps).forEach(depName => {
-      if(ignore.indexOf(depName) === -1) {
-        exec(`pnpm add ${depName}@${deps[depName]}`, (err, output) => {
+      if(ignoreList.indexOf(depName) === -1) {
+        exec(`pnpm add --offline --ignore-scripts ${depName}@${deps[depName]}`, (err, output) => {
           console.log(chalk.bold(`[Kiwi Bundle] Adding "${depName}" package...`))
           console.log(output)
         })
@@ -20,22 +20,22 @@ export const PostInstall = (path: string) => {
   commandExists("pnpm").then(exists => {
     if(exists) {
       const bundle = new Bundle(path)
-      const currentDepNames: string[] = []
+      const ignoreList: string[] = []
       const currentPackageJson = bundle.getPackageJson()
       if(typeof currentPackageJson.dependencies !== "undefined") {
         Object.keys(currentPackageJson.dependencies).forEach(packageName => {
-          currentDepNames.push(packageName)
+          ignoreList.push(packageName)
         })
       }
       if(typeof currentPackageJson.devDependencies !== "undefined") {
         Object.keys(currentPackageJson.devDependencies).forEach(packageName => {
-          currentDepNames.push(packageName)
+          ignoreList.push(packageName)
         })
       }
       Object.keys(bundle.dependencies).forEach(packageName => {
         if(packageName !== bundle.name) {
-          addDeps(bundle.dependencies[packageName].dependencies, currentDepNames)
-          addDeps(bundle.dependencies[packageName].devDependencies, currentDepNames)
+          addDeps(bundle.dependencies[packageName].dependencies, ignoreList)
+          addDeps(bundle.dependencies[packageName].devDependencies, ignoreList)
         }
       })
     }
