84a95b46fa4f2836058691dbf38ee559adfc0adf
Sylvain Cuomo <sc96@hotmail.fr>
2020-06-26 18:42:30 +0200

    Sandboxing

--- src/commands/postinstall.ts
+++ src/commands/postinstall.ts
@@ -3,6 +3,19 @@ import { exec } from "child_process"
 import { Bundle } from "../core/bundle"
 import chalk from "chalk"
 
+const addDeps = (deps: { [name: string]: string }, ignore: string[]) => {
+  if(typeof deps !== "undefined") {
+    Object.keys(deps).forEach(depName => {
+      if(ignore.indexOf(depName) === -1) {
+        exec(`pnpm add ${depName}@${deps[depName]}`, (err, output) => {
+          console.log(chalk.bold(`[Kiwi Bundle] Adding "${depName}" package...`))
+          console.log(output)
+        })
+      }
+    })
+  }
+}
+
 export const PostInstall = (path: string) => {
   commandExists("pnpm").then(exists => {
     if(exists) {
@@ -21,15 +34,8 @@ export const PostInstall = (path: string) => {
       }
       Object.keys(bundle.dependencies).forEach(packageName => {
         if(packageName !== bundle.name) {
-          const deps = bundle.dependencies[packageName].dependencies
-          Object.keys(deps).forEach(depName => {
-            if(currentDepNames.indexOf(depName) === -1) {
-              exec(`pnpm add ${depName}@${deps[depName]}`, (err, output) => {
-                console.log(chalk.bold(`[Kiwi Bundle] Adding "${depName}" package...`))
-                console.log(output)
-              })
-            }
-          })
+          addDeps(bundle.dependencies[packageName].dependencies, currentDepNames)
+          addDeps(bundle.dependencies[packageName].devDependencies, currentDepNames)
         }
       })
     }
