6b3bd9c421ec346725d1fe42ff5c4e8695cb3d37
Nazim Lachter <nazim@blueforest.cc>
2020-06-29 12:46:09 +0200

    Sandboxing

--- src/commands/postinstall.ts
+++ src/commands/postinstall.ts
@@ -1,43 +1,41 @@
-import commandExists from "command-exists"
+import { join } from "path"
 import chalk from "chalk"
+import { existsSync } from "fs"
 import { exec } from "child_process"
 import { Bundle } from "../core/bundle"
 
-const addDeps = (deps: { [name: string]: string }|undefined, ignoreList: string[]) => {
-  if(typeof deps !== "undefined") {
-    Object.keys(deps).forEach(depName => {
-      if(ignoreList.indexOf(depName) === -1) {
-        exec(`pnpm add --offline --ignore-scripts ${depName}@${deps[depName]}`, (err, output) => {
-          console.log(chalk.bold(`[Kiwi Bundle] Adding "${depName}" package...`))
-          console.log(output)
-        })
+export const PostInstall = (path: string) => {
+  if(existsSync(join(path, "pnpm-lock.yaml"))) {
+    const bundle = new Bundle(path)
+    const ignoreList: string[] = []
+    const currentPackageJson = bundle.getPackageJson()
+    if(typeof currentPackageJson.dependencies !== "undefined") {
+      Object.keys(currentPackageJson.dependencies).forEach(packageName => {
+        ignoreList.push(packageName)
+      })
+    }
+    if(typeof currentPackageJson.devDependencies !== "undefined") {
+      Object.keys(currentPackageJson.devDependencies).forEach(packageName => {
+        ignoreList.push(packageName)
+      })
+    }
+    Object.keys(bundle.dependencies).forEach(packageName => {
+      if(packageName !== bundle.name) {
+        const deps = bundle.dependencies[packageName].dependencies
+        if(typeof deps !== "undefined") {
+          Object.keys(deps).forEach(depName => {
+            if(ignoreList.indexOf(depName) === -1) {
+              console.log(existsSync(join(path, "node_modules", depName)) ? "OK" : depName)
+              if(!existsSync(join(path, "node_modules", depName))) {
+                exec(`pnpm add --prefer-offline --ignore-scripts --save-optional ${depName}@${deps[depName]}`, (err, output) => {
+                  console.log(chalk.bold(`[Kiwi Bundle] Adding "${depName}" package...`))
+                  console.log(output)
+                })
+              }
+            }
+          })
+        }
       }
     })
   }
-}
-
-export const PostInstall = (path: string) => {
-  commandExists("pnpm").then(exists => {
-    if(exists) {
-      const bundle = new Bundle(path)
-      const ignoreList: string[] = []
-      const currentPackageJson = bundle.getPackageJson()
-      if(typeof currentPackageJson.dependencies !== "undefined") {
-        Object.keys(currentPackageJson.dependencies).forEach(packageName => {
-          ignoreList.push(packageName)
-        })
-      }
-      if(typeof currentPackageJson.devDependencies !== "undefined") {
-        Object.keys(currentPackageJson.devDependencies).forEach(packageName => {
-          ignoreList.push(packageName)
-        })
-      }
-      Object.keys(bundle.dependencies).forEach(packageName => {
-        if(packageName !== bundle.name) {
-          addDeps(bundle.dependencies[packageName].dependencies, ignoreList)
-          addDeps(bundle.dependencies[packageName].devDependencies, ignoreList)
-        }
-      })
-    }
-  })
 }
