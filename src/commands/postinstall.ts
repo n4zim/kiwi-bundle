d1671222a88f797dc338f8f9934b50350dcf4901
Nazim Lachter <nazim@blueforest.fr>
2020-10-05 18:35:44 +0200

    recursive template copy

--- src/commands/postinstall.ts
+++ src/commands/postinstall.ts
@@ -1,9 +1,21 @@
 import { join } from "path"
-import { existsSync, readdirSync, statSync } from "fs"
+import { existsSync, readdirSync, statSync, readFileSync, writeFileSync, mkdirSync } from "fs"
 import { Bundle, KiwiBundlePackage } from "../core/bundle"
-import { readFileSync } from "fs"
-import { writeFileSync } from "fs"
-import { mkdirSync } from "fs"
+
+const walk = (templateRoot: string, destinationRoot: string, action: (path: string, content?: string) => void) => {
+  readdirSync(templateRoot).forEach(subPath => {
+    const destinationPath = join(destinationRoot, subPath)
+    if(!existsSync(destinationPath)) {
+      const templatePath = join(templateRoot, subPath)
+      if(statSync(templatePath).isDirectory()) {
+        action(destinationPath)
+        walk(templatePath, destinationPath, action)
+      } else {
+        action(destinationPath, readFileSync(templatePath, "utf-8"))
+      }
+    }
+  })
+}
 
 export const PostInstall = (path: string) => {
   const bundle = new Bundle(path)
@@ -13,21 +25,13 @@ export const PostInstall = (path: string) => {
     if(existsSync(templatePath)) {
       const appJson = JSON.parse(readFileSync(join(templatePath, "app.json"), "utf-8"))
       const appOptions = bundle.getCurrentOptions().app
-      const writeFileContent = (path: string, content: string) => {
+      walk(templatePath, bundle.path, (path, content) => {
         path = path.replace(appJson.name, appOptions.id)
-        content = content.replace(appJson.name, appOptions.id).replace(appJson.displayName, appOptions.name)
-        writeFileSync(path, content)
-      }
-      readdirSync(templatePath).forEach(subPath => {
-        const destinationPath = join(bundle.path, subPath)
-        if(!existsSync(destinationPath)) {
-          const templateSubPath = join(templatePath, subPath)
-          if(statSync(templateSubPath).isDirectory()) {
-            mkdirSync(destinationPath)
-            console.log(templateSubPath)
-          } else {
-            writeFileContent(destinationPath, readFileSync(templateSubPath, "utf-8"))
-          }
+        if(typeof content === "undefined") {
+          mkdirSync(path)
+        } else {
+          content = content.replace(appJson.name, appOptions.id).replace(appJson.displayName, appOptions.name)
+          writeFileSync(path, content)
         }
       })
     }
