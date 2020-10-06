71f8499c5af74f2b7f06f1794497be965d74cbf6
Nazim Lachter <nazim@blueforest.cc>
2020-10-06 23:06:05 +0200

    args on start

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -8,12 +8,14 @@ export const Start = (path: string) => {
   bundle.display()
 
   const reactHandler = bundle.getPackageHandler(KiwiBundlePackage.REACT, "start")
+
   if(typeof reactHandler !== "undefined") {
     reactHandler({
       path,
       outDir: bundle.compiler.outDir,
       options: bundle.getCurrentOptions(),
       handlers: bundle.getCurrentHandlers(),
+      args: process.argv.slice(3),
     })
   } else {
     const apiHandler = bundle.getPackageHandler(KiwiBundlePackage.API, "start")
