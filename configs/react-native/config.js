c9bec17e9765b311e88f69995bd4176f5f1db76c
Nazim Lachter <nazim@blueforest.cc>
2020-11-12 18:41:06 +0100

    eslint configs

--- configs/react-native/config.js
+++ configs/react-native/config.js
@@ -1,10 +1,12 @@
 module.exports = {
   dependencies: {
-    "react-native": {},
-    "react-native-reanimated": {},
-    "react-native-gesture-handler": {},
-    "react-native-screens": {},
-    "react-native-safe-area-context": {},
+    "@react-native-community/checkbox": {},
     "@react-native-community/masked-view": {},
+    "@react-native-community/picker": {},
+    "react-native": {},
+    "react-native-gesture-handler": {},
+    "react-native-reanimated": {},
+    "react-native-safe-area-context": {},
+    "react-native-screens": {},
   },
 }
